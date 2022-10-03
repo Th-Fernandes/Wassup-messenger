4d43171e389ce053bc5f6388652aacb28ca27778
Thiago Fernandes <tjfctigo@gmail.com>
2022-10-03 11:23:59 -0300

    NextImage component

---
+++ src/components/Next/Image.tsx
@@ -0,0 +1,22 @@
+import Image from "next/future/image";
+
+interface ImgProps {
+  src: string;
+  alt: string | undefined;
+  className?: string;
+  width: string | number;
+  height: string | number;
+}
+
+export function NextImage({src, alt, className, width, height}:ImgProps) {
+  return (
+    <Image 
+      sizes="auto"
+      width={width || 64}
+      height={height || 64}
+      className={className}
+      src={src}
+      alt={alt}
+    />
+  );
+}
--- src/pages/Index/SignAction/AsideInfo/index.tsx
+++ src/pages/Index/SignAction/AsideInfo/index.tsx
@@ -1,15 +1,28 @@
+import { NextImage } from "components/Next/Image";
+
 export function AsideInfo() {
   return (
     <aside className="hidden lg:flex flex-col justify-between items-center py-32 w-[36.85%]  bg-brand-hover">
       <div className="text-light-txt-100  text-center">
-        <img className="block mx-auto" src="/wassup-logo.svg" alt="logo" />
+        <NextImage 
+          className="block mx-auto" 
+          src="/wassup-logo.svg" 
+          alt="logo" 
+          width={256}
+          height={256}
+        />
 
         <h2 className="text-4xl font-bold mb-2">Wassup Messenger</h2>
         <p >O seu mensageiro. Simples, Rápido e fácil.</p>
       </div>
 
       <div className="text-light-txt-100">
-        <img className="block mx-auto" src="/qrcode-asset.png" alt="escaneie o QRcode para baixar o aplicativo" />
+        <NextImage 
+          className="block mx-auto" 
+          src="/qrcode-asset.png" 
+          alt="escaneie o QRcode para baixar o aplicativo" 
+          width={180} height={180}
+        />
         <p className="text-sm text-light-txt-200 mt-2">Experimente nossa versão mobile</p>
       </div>
     </aside>
