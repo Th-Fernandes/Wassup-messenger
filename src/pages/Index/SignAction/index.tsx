f3c42937a027f50d99f575e60f3ed6f37b0e201b
Thiago Fernandes <tjfctigo@gmail.com>
2022-10-07 07:00:11 -0300

    delete: old email confirm modal

--- src/pages/Index/SignAction/index.tsx
+++ src/pages/Index/SignAction/index.tsx
@@ -1,14 +1,10 @@
-import { useState } from "react";
+
 import { AsideInfo } from "./AsideInfo";
 import { SignTypeToggle } from "./SignTypeToggle";
-import { EmailConfirm } from "./SignTypeToggle/EmailConfirm";
 
 export function SignAction() {
-  const [emailModal, setEmailModal] = useState(false);
-
   return (
     <>
-      {emailModal && <EmailConfirm emailModal={setEmailModal}/>}
       <AsideInfo />
       <SignTypeToggle/>
     </>
