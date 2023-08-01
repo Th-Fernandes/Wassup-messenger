f98f4d3ccdc5c209706b57a257fe8567f22b5c2d
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-08-01 15:46:35 -0300

    fix: Next Image Props overwrite

--- src/components/Next/Image.tsx
+++ src/components/Next/Image.tsx
@@ -2,21 +2,23 @@ import Image from "next/image";
 
 interface ImgProps {
   src: string;
-  alt: string | undefined;
+  alt: string;
   className?: string;
-  width: string | number;
-  height: string | number;
+  width: number | `${number}` | undefined
+  height: number | `${number}` | undefined;
 }
 
-export function NextImage({src, alt, className, width, height}:ImgProps) {
+
+
+export function NextImage({src, alt, className, width, height}: ImgProps) {
   return (
     <Image 
-      sizes="auto"
-      width={width || 64}
-      height={height || 64}
       className={className}
       src={src}
       alt={alt}
+      sizes="auto"
+      width={width || 64}
+      height={height || 64}
     />
   );
 }
