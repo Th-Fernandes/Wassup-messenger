94edf5b10564584d2e1feb3aada54083741e80cf
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-08-10 16:29:35 -0300

    fix: use router hook from /navigate

--- app/chat/components/SignOutDialog.tsx
+++ app/chat/components/SignOutDialog.tsx
@@ -1,5 +1,5 @@
 import { Dialog } from "components/radix/Dialog";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { supabase } from "utils/supabaseClient";
 
 
